b0412ca6aefdd84032209bcdb58f8912e6b80e66
basitol <basitolaitan27@gmail.com>
2022-12-27 23:49:30 +0100

    update : app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Greet,
+  Person,
+  PersonList,
+  Status,
+  Validation,
+  Header,
+  Oscar,
+  Button,
+  Input,
+  Container,
+  LoggedIn,
+} from "./components";
 
 function App() {
+  const myName = {
+    first: "Abdulbasit",
+    last: "Quadri",
+  };
+
+  const nameList = [
+    {
+      first: "Abdulbasit",
+      last: "Quadri",
+    },
+    {
+      first: "Aminu",
+      last: "Kareem",
+    },
+    {
+      first: "Abdulhakeem",
+      last: "Quadri",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container
+        styles={{
+          border: "1px solid black",
+          padding: "10px",
+          margin: "10px",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow: "1px 1px 1px #111",
+          backgroundColor: "#f5f5f5",
+        }}
+      >
+        <LoggedIn />
+        <Greet
+          name="Abdulbasit"
+          country="Nigeria"
+          isLoggedIn
+          age={1}
+          messageCount={4}
+        />
+        <Person name={myName} />
+        <PersonList names={nameList} />
+        <Status status="success" />
+        <Validation />
+        <Header>Header</Header>
+        <Oscar>
+          <Header>Oscar goes to Leonardo Decarprio!</Header>
+        </Oscar>
+        <Button
+          handleClick={(event, id) => console.log("Button Clicked", event, id)}
+        />
+        <Input
+          handleChange={(event) => console.log(event.target.value)}
+          value=""
+        />
+      </Container>
     </div>
   );
 }
