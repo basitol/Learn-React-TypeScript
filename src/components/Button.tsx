205714a0fde2f32b211c3254634fb475e0461a85
basitol <basitolaitan27@gmail.com>
2022-12-27 23:50:12 +0100

    build : button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type ButtonProps = {
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id: number) => void;
+};
+
+const Button = ({ handleClick }: ButtonProps) => {
+  return <button onClick={(event) => handleClick(event, 1)}>Click Me</button>;
+};
+
+export default Button;
